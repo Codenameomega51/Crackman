f44f6b40387928d8ebcfc05dc9072e2879f280e4
Grechelle Uy <grechelleuy@yahoo.com>
2022-11-25 00:27:37 -0800

    ghost gif 3/5 complete

--- Ghost3.java
+++ Ghost3.java
@@ -1,4 +1,3 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
 import lang.stride.*;
 import java.util.*;
 import greenfoot.*;
@@ -8,13 +7,17 @@ import greenfoot.*;
  */
 public class Ghost3 extends Ghosts
 {
-
+public GifImage Ghost1 =  new  GifImage("Ghost3Gif.gif");
     /**
      * Act - do whatever the Ghost3 wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        mouvement();
+       if (!(win())){
+            mouvement();
+        }
+        wallCollide();
+        setImage(Ghost1.getCurrentImage());
     }
 
     /**
@@ -23,5 +26,39 @@ public class Ghost3 extends Ghosts
     public void mouvement()
     {
         move(3);
+    }
+       /**
+     * 
+     */
+    public void wallCollide()
+    {
+        int x = getX();
+        int y = getY();
+        if (isTouching(Wall_Right.class)) {
+            setLocation(getX() - 10, getY() - 10);
+            turn(90);
+        }
+        if (isTouching(Wall_Up.class)) {
+            setLocation(getX() + 3, getY() + 3);
+        }
+        if (isTouching(Wall_Down.class)) {
+            setLocation(getX() - 3, getY() - 3);
+        }
+        if (isTouching(Wall_Left.class)) {
+            setLocation(getX() + 10, getY() + 10);
+            turn(90);
+        }
+        if (isTouching(Wall.class)) {
+            setLocation(getX() - 10, getY() + 2);
+        }
+    }
+
+    public boolean win(){
+        boolean win = false;
+        Actor cracked = getOneIntersectingObject(CrackMan.class);
+        if (cracked != null){
+            win = true;
+        }
+        return win;
     }
 }
