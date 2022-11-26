66093c8a3d610b1949868aa611d0263c2d776d16
Codenameomega51 <youssefabdelnour2004@yahoo.com>
2022-11-25 21:07:47 -0500

    Made the ghosts that get faster by each win

--- Ghost1.java
+++ Ghost1.java
@@ -8,7 +8,7 @@ import greenfoot.*;
 public class Ghost1 extends Ghosts
 {
     public GifImage Ghost1 =  new  GifImage("Ghost1.gif");
-
+    int speed = 3;
     /**
      * 
      */
@@ -33,8 +33,10 @@ public class Ghost1 extends Ghosts
      */
     public void mouvement()
     {
-        move(3);
-
+        move(speed);
+        if (getWorld() instanceof level_Complete){
+            speed += 1;
+        }
     }
 
     /**
