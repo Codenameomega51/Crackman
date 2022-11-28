27e405fb2ce1c2d118f93c1e475696e9d20f73a8
Codenameomega51 <112569045+Codenameomega51@users.noreply.github.com>
2022-11-28 13:43:20 -0500

    temp loss screen

--- Lost.java
+++ Lost.java
@@ -7,12 +7,20 @@ import greenfoot.*;
  */
 public class Lost extends World
 {
-
+    GreenfootSound lost = new GreenfootSound("fail.mp3");
+    int timer = 0;
     /**
      * Constructor for objects of class Lost.
      */
     public Lost()
     {
         super(900, 800, 1);
+    }
+
+    public void act() {
+        if (timer == 0){
+            lost.play();
+            timer++;
+        }
     }
 }
